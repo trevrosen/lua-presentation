bd1e54b6b3d6af0b032027e7ad0116a19fc58904
Trevor Rosen <trevor@catapult-creative.com>
2012-11-12 21:20:34 -0600

    More cleanup of hello.lua

--- hello.lua
+++ hello.lua
@@ -1,26 +1,22 @@
---Variables are global in Lua by default
---like in JS, you use a keyword to limit to the current scope
+-- Take a Twitter handle interactively, grab the feed
+-- and optionally dump the raw HTML to the console.
 
-local io   = require('io') --get an IO object from the stdlib
-local http = require('socket.http') --from LuaSocket library installed via luarocks
+local io   = require('io')          -- from the stdlib
+local http = require('socket.http') -- from LuaSocket library installed via luarocks
 
 print("Whose Twitter feed do you want to retrieve?")
 local username = io.read() -- read from stdin like Ruby's "gets"
 
---now we'll make a request to Twitter
 local url = "http://twitter.com/"..username
-print("Sending request to "..url.."...")
+print(string.format("Sending request to %s",url))
 
---response contains entire response body
---when you use it in this simple way
-local response = http.request(url)
+local response = http.request(url) -- make the GET request
 
 print("Got it.  Do you want to print it to the console?")
 
 local pref = io.read()
 
--- pattern-match for 'Y' or 'y' in response
-if string.find(pref, '[Yy]') ~= nil then
+if string.match(pref, '[Yy]') ~= nil then
   print(response)
 else
   print("OK then, be that way.")
