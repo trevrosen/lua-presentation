53aa5c122313354daaebd3a10348c60e15c8bc5a
Trevor Rosen <trevor@catapult-creative.com>
2012-11-12 21:01:42 -0600

    Demo some string stuff

--- foobaricus.lua
+++ foobaricus.lua
@@ -7,10 +7,10 @@
 -- unless the expression is true, an error is raised
 assert(foobaricus, "you must have a 'foobaricus' table in your environment")
 
-foobaricus.fun()
+foobaricus.fun() -- will throw error unless foobaricus.fun has been defined
 
 foobaricus.zanzibar = function (name)
-  print("We are taking "..name.." to Zanzibar")
+  print(string.format("We are taking %s to Zanzibar", name)) -- works like printf
 end
 
 
